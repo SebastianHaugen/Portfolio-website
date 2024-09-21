4e573b59bc8c1f629efae0328f528ab1a1089cbe
SebastianHaugen <sebassha@stud.ntnu.no>
2024-09-21 12:18:46 +0200

    Working on the project list

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,5 +1,6 @@
 import "./styles/projects.css";
 import fullstack from "../assets/img/full-stack-application.png";
+import travel from "../assets/img/travel.png";
 
 function Projects() {
   return (
@@ -25,21 +26,39 @@ function Projects() {
 
       <div className="project project2">
         <div className="project-content">
-          <h2>Snowplowing for Gjøvik county</h2>
-          <img src="https://via.placeholder.com/150" alt="project 2" />
+          <img src={travel} alt="project 2" />
         </div>
         <div className="project-text">
-          <p>Project 2 description</p>
+          <h2>Accessible website</h2>
+          <p>
+            This project is a fully accessible, travel-themed website developed
+            as part of the IDG2012 course. The primary goal was to create a
+            user-friendly platform for young adults (aged 18-35) that adheres to
+            the Web Content Accessibility Guidelines (WCAG). The design focuses
+            on inclusivity, ensuring that users with varying disabilities can
+            easily navigate and interact with the content. ARIA Landmarks and
+            Roles: ARIA (Accessible Rich Internet Applications) roles and
+            landmarks were added where necessary to provide additional context
+            for assistive technologies, ensuring that dynamic elements like
+            interactive forms are accessible. Keyboard Navigation: The entire
+            website can be navigated using only a keyboard, making it accessible
+            to users with motor impairments. Focus styles are clearly visible to
+            ensure users can always identify where they are on the page. Color
+            Contrast and Font Size: The website was designed with high-contrast
+            color schemes and resizable text, providing readability for users
+            with visual impairments. All elements are responsive to zoom and
+            screen resizing without loss of functionality.
+          </p>
         </div>
       </div>
 
       <div className="project project3">
         <div className="project-content">
-          <h2>Sustainbable and accessible website</h2>
-          <img src="https://via.placeholder.com/150" alt="project 3" />
+          <h2>Snowplowing for Gjøvik county</h2>
+          <img src="https://via.placeholder.com/150" alt="project 2" />
         </div>
         <div className="project-text">
-          <p>Project 3 description</p>
+          <p></p>
         </div>
       </div>
     </div>
