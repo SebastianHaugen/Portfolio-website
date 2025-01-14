e1a6c01d7dbc09dbba913e9ce0d267fbb5f51d29
Sebastian Haugen <126162894+SebastianHaugen@users.noreply.github.com>
2025-01-14 12:26:39 +0100

    Small typo fixes

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -13,12 +13,13 @@ function About() {
         bachelor i 2025.
       </p>
       <p>
-        Jeg er fra Bødalen i Asker, Norge. Jeg har alltid hatt en lidenskap for
-        teknologi og koding. Jeg syns det er gøy å lære nye ting og utfordre meg
-        selv. Jeg syns det er gøy å lage nettsider fra bunnen av. Å holde meg
-        aktiv er viktig for meg. Jeg liker å bruke tid på treningssenter. I
-        tillegg er jeg glad i å reise og det å oppleve nye steder og kulturer.
-        Jeg liker å ta bilder av stedene jeg besøker.
+        Jeg er 23 år gammel og kommer fra Bødalen i Asker. Teknologi og koding
+        har alltid vært en lidenskap for meg. I 2022 begynte jeg på studiet
+        webutvikling ved NTNU og jeg innså raskt at dette var noe jeg virkelig
+        ønsket å jobbe med. Jeg liker å lære nye ting og utfordre meg selv. Det
+        er også viktig for meg å holde meg aktiv, og jeg bruker mye tid på
+        treningssenteret. I tillegg elsker jeg å reise og oppleve nye steder og
+        kulturer.
       </p>
       <div className="image-collage">
         <img src={gaustatoppen} alt="Toppen av Gaustatoppen, Norge" />
--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -17,15 +17,16 @@ function Projects() {
             For IDG2100-emnet utviklet jeg og to andre studenter, en fullstack
             webapplikasjon kalt SUPER Assessor. Applikasjonen er designet for
             lærere, slik at de kan lage og evaluere vurderingsskjemaer gjennom
-            et kortbasert spill. Lærere kan spille alene eller i team og spillet
-            resulterer i et nytt vurderingsskjema som kan deles med andre eller
-            lagres til senere bruk. Applikasjonen lar også lærere administrere
-            sine egne profiler, spilløkter og generere PDF-er av skjemaene sine.
-            Vi brukte MERN-stacken for å utvikle applikasjonen. Jeg bidro til å
-            designe REST API for backend, bygge frontend med React og
-            implementere sikkerhetsfunksjoner for innlogging med tokens. Dette
-            prosjektet lærte vi å jobbe med alle deler av et prosjekt både
-            frontend og backend for å bygge en skalerbar og sikker applikasjon.
+            et kortbasert spill. Lærere kan spille alene eller i et team. Når
+            spillet er ferdig er resultatet et nytt vurderingsskjema som kan
+            deles med andre eller lagres til senere bruk. Applikasjonen lar også
+            lærere administrere sine egne profiler, spilløkter og generere
+            PDF-er av skjemaene sine. Vi brukte MERN-stacken for å utvikle
+            applikasjonen. Jeg bidro til å designe REST API for backend, bygge
+            frontend med React og implementere sikkerhetsfunksjoner for
+            innlogging med tokens. I dette prosjektet lærte vi å jobbe med alle
+            deler av et prosjekt, både frontend og backend, for å bygge en
+            skalerbar og sikker applikasjon..
           </p>
         </div>
       </div>
@@ -37,16 +38,16 @@ function Projects() {
         <div className="project-text">
           <h3>Reise nettside med fokus på tilgjengelighet</h3>
           <p>
-            For IDG2012-emnet utviklet jeg en nettside med et reisetema. Målet
-            var å lage en brukervennlig plattform for unge voksne som følger
+            I IDG2012-emnet utviklet jeg en nettside med et reisetema. Målet var
+            å lage en brukervennlig plattform for unge voksne, i tråd med
             retningslinjene for tilgjengelig webinnhold (WCAG). Designet
-            fokuserer på inkludering og sikrer at brukere med ulike
-            funksjonshemninger enkelt kan navigere og samhandle med innholdet.
-            Jeg har brukt ARIA der det var nødvendig for å gi ekstra kontekst
-            for hjelpemiddelteknologier og passe på at alle linker er
-            beskrivende. Hele nettstedet kan navigeres ved hjelp av kun
+            fokuserte på inkludering og sørget for at brukere med ulike
+            funksjonsnedsettelser enkelt kunne navigere og interagere med
+            innholdet. Jeg brukte ARIA der det var nødvendig for å gi ekstra
+            kontekst til hjelpemiddelteknologier og sørget for at alle lenker
+            var beskrivende. Hele nettstedet kan navigeres kun ved hjelp av
             tastatur, noe som gjør det tilgjengelig for brukere med motoriske
-            funksjonshemninger.
+            funksjonsnedsettelser.
           </p>
         </div>
       </div>
@@ -58,16 +59,15 @@ function Projects() {
         <div className="project-text">
           <h3>TryggTur for Gjøvik kommune</h3>
           <p>
-            For IDG2000-emnet. Utviklet vi en tjeneste designet for å informere
-            og be om måking av fortau. Brukere kan abonnere på varsler når
-            fortau vil bli strødd eller måket for snø og de kan også sende
+            For IDG2000-emnet utviklet vi en tjeneste designet for å informere
+            om og be om måking av fortau. Brukere kan abonnere på varsler når
+            fortau blir strødd eller måket for snø, og de kan også sende
             forespørsler om disse tjenestene i sitt eget nabolag eller andre
-            steder i byen. Målet er å gjøre det tryggere å gå og sykle i byen om
-            vinteren. Nettsiden inkluderer også en seksjon som fremmer tryggere
-            kjørevaner for å beskytte fotgjengere og syklister. Appen har som
-            mål å forbedre kommunikasjonen mellom innbyggere og kommunen. Slik
-            at kommunen vet hvor det er dårlig måkt. Appen har som mål og
-            oppmuntrer flere til å gå eller sykle om vinteren.
+            steder i byen. Målet med tjenesten er å gjøre det tryggere å gå og
+            sykle i byen om vinteren. Nettsiden inkluderer også en seksjon som
+            fremmer tryggere kjørevaner for å beskytte fotgjengere og syklister.
+            Appen har som mål å forbedre kommunikasjonen mellom innbyggere og
+            kommunen, slik at kommunen vet hvor det er dårlig måkt.
           </p>
         </div>
       </div>
