8f9174ff81797d3d7ab286fbfc8a8a8fd3a58ef2
Sebastian Haugen <126162894+SebastianHaugen@users.noreply.github.com>
2025-02-13 06:22:48 +0100

    Website has been translated to English

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -1,26 +1,17 @@
 import gaustatoppen from "../assets/img/webp/gaustatoppen-min.webp";
 import prague from "../assets/img/webp/prague-min.webp";
 import prague2 from "../assets/img/webp/prague2-min.webp";
+import { useTranslation } from "react-i18next";
 import "./styles/about.css";
 
 function About() {
+  const { t } = useTranslation();
+
   return (
     <div className="about-container" id="about">
       <h2 className="about-title">Om</h2>
-      <p className="about-header">
-        Jeg er en <b>utvikler</b> og en <b>designer</b> som har studert
-        webutvikling ved NTNU Gjøvik de siste 3 årene. Jeg er ferdig med
-        bachelor i 2025.
-      </p>
-      <p>
-        Jeg er 23 år gammel og kommer fra Bødalen i Asker. Teknologi og koding
-        har alltid vært en lidenskap for meg. I 2022 begynte jeg på studiet
-        webutvikling ved NTNU og jeg innså raskt at dette var noe jeg virkelig
-        ønsket å jobbe med. Jeg liker å lære nye ting og utfordre meg selv. Det
-        er også viktig for meg å holde meg aktiv, og jeg bruker mye tid på
-        treningssenteret. I tillegg elsker jeg å reise og oppleve nye steder og
-        kulturer.
-      </p>
+      <p className="about-header">{t("about.description1")}</p>
+      <p>{t("about.description2")}</p>
       <div className="image-collage">
         <img src={gaustatoppen} alt="Toppen av Gaustatoppen, Norge" />
         <img src={prague} alt="Praha Kirke Basilica of St. Ludmila" />
--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,13 +1,16 @@
 import github from "../assets/img/GitHub_Logo.png";
 import linkedin from "../assets/img/LI-Logo.png";
+import { useTranslation } from "react-i18next";
 import "./styles/contact.css";
 
 function Contact() {
+  const { t } = useTranslation();
+
   return (
     <div className="contact-container" id="contact">
-      <h2>Kontakt</h2>
-      <p>Du kan kontakte meg på:</p>
-      <p>e-post: 
+      <h2>{t("contact.header")}</h2>
+      <p>{t("contact.description")}</p>
+      <p>{t("contact.email")}
         <a href="mailto:sebastian@haug1.net">sebastian@haug1.net</a>
       </p>
       <div className="contact-links">
